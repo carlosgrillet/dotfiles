065a863e5c74950e83ab1aa455034e187467b4b3
carlosgrillet <carlosgrillet5000@hotmail.com>
2022-02-06 04:19:01 -0400

    terimal icon added to user profile

--- powershell/user_profile.ps1
+++ powershell/user_profile.ps1
@@ -6,6 +6,9 @@ Set-PoshPrompt Paradox
 # SET THEME ON LOAD
 oh-my-posh --init --shell pwsh --config C:\Users\Carlos\.config\powershell\carlos.omp.json | Invoke-Expression
 
+# ICONS
+Import-Module -Name Terminal-Icons
+
 # ALIAS
 Set-Alias vim nvim
 Set-Alias g git
