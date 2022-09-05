f0d5bc2b390c2e7f584ff8cbc9328181d516784b
Carlos Grillet <carlosgrillet5000@hotmail.com>
2022-09-05 16:45:27 -0400

    added splitbelow and splitright, and color column 80

--- .config/nvim/lua/base.lua
+++ .config/nvim/lua/base.lua
@@ -5,13 +5,17 @@ vim.opt.encoding = 'utf-8'
 vim.opt.fileencoding = 'utf-8'
 
 vim.wo.number = true
+vim.wo.relativenumbner = true
 
+vim.opt.splitright = true
+vim.opt.splitbelow = true
 vim.opt.title = true
 vim.opt.undofile = true
 vim.opt.undodir = '~/.vim/undo'
 vim.opt.autoindent = true
 vim.opt.smartindent = true
 vim.opt.hlsearch = true
+vim.opt.colorcolumn = '80'
 vim.opt.backup = false
 vim.opt.showcmd = true
 vim.opt.cmdheight = 1
