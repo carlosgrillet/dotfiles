dcff347f020dda046324df076a02b9ec313244fc
Carlos Grillet <carlosgrillet5000@hotmail.com>
2022-12-20 12:08:26 -0400

    New maps added from the primeagen

--- .config/nvim/lua/maps.lua
+++ .config/nvim/lua/maps.lua
@@ -6,15 +6,8 @@ keymap.set('n', 'x', '"_x')
 keymap.set('n', '+', '<C-a>')
 keymap.set('n', '-', '<C-x>')
 
--- Delete a word backwards
--- keymap.set('n', 'dw', 'vb"_d')
-
 -- Select all
 keymap.set('n', '<C-a>', 'gg<S-v>G')
-
--- Save with root permission (not working for now)
---vim.api.nvim_create_user_command('W', 'w !sudo tee > /dev/null %', {})
-
 -- New tab
 keymap.set('n', 'te', ':tabedit', { silent = true})
 -- Split window
@@ -33,3 +26,20 @@ keymap.set('n', '<C-w><left>', '<C-w><')
 keymap.set('n', '<C-w><right>', '<C-w>>')
 keymap.set('n', '<C-w><up>', '<C-w>+')
 keymap.set('n', '<C-w><down>', '<C-w>-')
+-- Move the selected lines
+keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
+keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
+-- Search in the middle of screen
+keymap.set('n', 'n', 'nzzzv')
+keymap.set('n', 'N', 'Nzzzv')
+-- Stay cursor when join lines
+keymap.set('n', 'J', 'mzJ`z')
+-- Keep curson in the middle when half-page jumping
+keymap.set('n', '<C-d>', '<C-d>zz')
+keymap.set('n', '<C-u>', '<C-u>zz')
+-- Yank to the clipboard
+keymap.set('n', '<Leader>y', '"+y')
+keymap.set('v', '<Leader>y', '"+y')
+keymap.set('n', '<Leader>Y', '"+Y')
+-- Paste from clipboard
+keymap.set('n', '<Leader>p', '"+gP')
