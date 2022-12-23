e8a3f66fd7061dac21d27c67e87123a22a6662d9
Carlos Grillet <carlosgrillet5000@hotmail.com>
2022-12-23 09:00:37 -0400

    luasnip plug added and plugins sorted

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -7,42 +7,44 @@ end
 vim.cmd [[packadd packer.nvim]]
 
 packer.startup(function(use)
-  use 'wbthomason/packer.nvim'
-  use {
-    'svrana/neosolarized.nvim',
-    requires = { 'tjdevries/colorbuddy.nvim' }
-  }
+  use 'wbthomason/packer.nvim' -- Packet manager for neovim
+  -- Theme, colors and icons
+  use { 'svrana/neosolarized.nvim', requires = { 'tjdevries/colorbuddy.nvim' } }
   use 'nvim-lualine/lualine.nvim' -- Statusline
   use 'nvim-lua/plenary.nvim' -- Common utilities
   use 'onsails/lspkind-nvim' -- vscode-like pictograms
+  use 'kyazdani42/nvim-web-devicons' -- File icons
+  use 'norcalli/nvim-colorizer.lua' -- Color highlighter
+  -- Completion tools
+  use 'hrsh7th/nvim-cmp' -- Completion
   use 'hrsh7th/cmp-buffer' -- nvim-cmp source for buffer words
   use 'hrsh7th/cmp-nvim-lsp' -- nvim-cmp source for neovim's built-in LSP
-  use 'hrsh7th/nvim-cmp' -- Completion
-  use 'williamboman/nvim-lsp-installer' -- Allow you to manage LSP servers
+  use 'hrsh7th/cmp-vsnip' -- nvim-cmp source for luasnip
+  use 'L3MON4D3/LuaSnip'
+  -- Language server tools
   use 'neovim/nvim-lspconfig' -- LSP
+  use 'williamboman/nvim-lsp-installer' -- Allow you to manage LSP servers
   use 'jose-elias-alvarez/null-ls.nvim' -- Use Neovim as a language server to inject LSP diagnostics, code actions, and more via Lua
   use 'MunifTanjim/prettier.nvim' -- Prettier plugin for Neovim's built-in LSP client
-  use 'williamboman/mason.nvim'
+  use 'williamboman/mason.nvim' -- UI for Mange LSP servers
   use 'williamboman/mason-lspconfig.nvim'
   use 'glepnir/lspsaga.nvim' -- LSP UIs
-  use 'L3MON4D3/LuaSnip'
+  -- Nvim tools
   use {
     'nvim-treesitter/nvim-treesitter',
-    run = function() require('nvim-treesitter.install').update({ with_sync = true }) end,
+    run = function() require('nvim-treesitter.install').update({
+      with_sync = true
+    }) end,
   }
-  use 'kyazdani42/nvim-web-devicons' -- File icons
-  use 'nvim-telescope/telescope.nvim'
-  use 'nvim-telescope/telescope-file-browser.nvim'
-  use 'windwp/nvim-autopairs'
-  use 'windwp/nvim-ts-autotag'
-  use 'norcalli/nvim-colorizer.lua'
+  use 'nvim-telescope/telescope.nvim' -- High extendable fuzzy finder
+  use 'nvim-telescope/telescope-file-browser.nvim' -- File fuzzy finder
+  use 'windwp/nvim-autopairs' -- Auto pair plugin
+  use 'windwp/nvim-ts-autotag' -- Autoclose and autorename html tags
   use({
     "iamcco/markdown-preview.nvim",
     run = function() vim.fn["mkdp#util#install"]() end,
   })
   use 'akinsho/nvim-bufferline.lua'
-  -- use 'github/copilot.vim'
-
   use 'lewis6991/gitsigns.nvim'
   use 'dinhhuy258/git.nvim' -- For git blame & browse
   use 'tpope/vim-surround' -- For operations surrounding
