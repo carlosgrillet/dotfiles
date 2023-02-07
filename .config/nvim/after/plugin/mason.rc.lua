e74db0328f536979f9901a113f44a9e353e91bf3
Carlos Grillet <carlosbeta5000@gmail.com>
2023-02-06 20:56:01 -0400

    pyright is now installed automatically

--- .config/nvim/after/plugin/mason.rc.lua
+++ .config/nvim/after/plugin/mason.rc.lua
@@ -3,10 +3,12 @@ if (not status) then return end
 local status2, lspconfig = pcall(require, "mason-lspconfig")
 if (not status2) then return end
 
-mason.setup({
-
-})
+mason.setup({})
 
 lspconfig.setup {
-  ensure_installed = { "sumneko_lua", "tailwindcss" },
+  ensure_installed = {
+    "sumneko_lua",
+    "tailwindcss",
+    "pyright",
+  },
 }
