61cc9bda708f96e2a4ed0c3a7c62372b59a97d74
Carlos Grillet <carlosbeta5000@gmail.com>
2023-03-09 19:02:45 -0400

    new theme and config added to powershell

--- .config/powershell/user_profile.ps1
+++ .config/powershell/user_profile.ps1
@@ -1,19 +1,64 @@
-# PROMPT
-Import-Module posh-git
-Import-Module oh-my-posh
-Set-PoshPrompt Paradox
-
-# SET THEME ON LOAD
-oh-my-posh --init --shell pwsh --config C:\Users\Carlos\.config\powershell\carlos.omp.json | Invoke-Expression
-
-# ICONS
-Import-Module -Name Terminal-Icons
-
-# ALIAS
-#function nvimcfg {nvim -u $HOME\.config\nvim\init.vim -}
-#Set-Alias vim nvimcfg
-
+# Alias
 Set-Alias vim nvim
-Set-Alias g git
 Set-Alias ll ls
+Set-Alias g git
 Set-Alias grep findstr
+
+# Import modules
+Import-Module Terminal-Icons
+Import-Module PSFzf
+
+
+# Config modules
+oh-my-posh init pwsh --config $env:USERPROFILE\.config\powershell\theme.omp.json | Invoke-Expression
+Set-PSReadLineOption -PredictionSource History
+Set-PSReadLineOption -BellStyle None 
+Set-PSFzfOption -PSReadlineChordProvider 'Ctrl+f' -PSReadlineChordReverseHistory 'Ctrl+r'
+
+#functions
+function Get-PublicIP(){
+  try{
+    (iwr -uri "ifconfig.me/ip").content
+  }
+  catch{
+    write-host -ForegroundColor Red "Connection Error"
+  }
+}
+
+function Get-PacketLoss($ip){
+  while(1){
+    ping -n 100 $ip |
+    Select-String "[0-9]{1,3}% perdidos" | 
+    foreach {$_.Matches.Value} | 
+    foreach {"{0} - {1}" -f (Get-Date),$_}
+  }
+}
+
+function Get-Latency($ip){
+  if ($ip -eq "") {
+      return 0
+  }
+  "{0}`t`t{1}`t{2}" -f "Time","RTT","%loss"
+  while(1) {
+      $latencia = ping -n 100 $ip 
+      $Media = $latencia | Select-String "Media = [0-9]{1,4}ms" | 
+      foreach {$_.Matches.Value} |
+      Select-String "[0-9]{1,4}" |
+      foreach {$_.Matches.Value}
+      $loss = $latencia | Select-String "[0-9]{1,3}% perdidos" | 
+      foreach {$_.Matches.Value} | 
+      Select-String "[0-9]{1,4}" |
+      foreach {$_.Matches.Value}
+
+      "{0}`t{1}`t{2}" -f (Get-Date -Format "HH:mm:ss"),$media,$loss
+  }
+}
+
+function router(){
+  ssh cgrillet@10.250.0.1
+}
+
+function vimdir(){
+  cd $env:LOCALAPPDATA/nvim
+  vim . 
+}
