0bb14ad1dc7871f8034acc8e5e48f9200e76add8
Carlos Grillet <13719979+carlosgrillet@users.noreply.github.com>
2023-06-19 15:51:27 -0400

    Update base.lua added guicursor same any time

--- .config/nvim/lua/base.lua
+++ .config/nvim/lua/base.lua
@@ -4,7 +4,7 @@ vim.scriptencoding = 'utf-8'
 vim.opt.encoding = 'utf-8'
 vim.opt.fileencoding = 'utf-8'
 
-vim.opt.guicursor = ""
+vim.opt.guicursor = "n-v-c-i:Block"
 vim.opt.mouse = ""
 vim.wo.number = true
 vim.wo.relativenumber = true
