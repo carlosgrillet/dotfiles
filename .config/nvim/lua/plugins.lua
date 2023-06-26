6adeeb61f18806ab41461f8d28abaeb958c77224
Carlos Grillet <carlosbeta5000@gmail.com>
2023-06-26 09:06:24 -0400

    better comments pluggin added

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -58,4 +58,5 @@ packer.startup(function(use)
   use 'tpope/vim-surround' -- For operations surrounding
   use 'tpope/vim-repeat' -- To repeat surround operations
   use 'mattn/emmet-vim'
+  use "Djancyp/better-comments.nvim" -- For better comment lines
 end)
