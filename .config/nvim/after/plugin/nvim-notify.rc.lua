81640fb78bd1cfc0a3e96fa173597c47f73f3ace
Carlos Grillet <carlosbeta5000@gmail.com>
2024-01-13 16:22:59 +0100

    fix: notify not wrapping the content

--- .config/nvim/after/plugin/nvim-notify.rc.lua
+++ .config/nvim/after/plugin/nvim-notify.rc.lua
@@ -2,16 +2,9 @@ local status, notify = pcall(require, "notify")
 if (not status) then return end
 
 notify.setup({
-  render = "compact",
+  max_width = 80,
   fps = 60,
   stages = "fade",
-  max_width = 80,
   timeout = 500,
-  icons = {
-    ERROR = "",
-    WARN = "",
-    INFO = "",
-    DEBUG = "",
-    TRACE = "✎",
-  },
+  render = "wrapped-compact",
 })
