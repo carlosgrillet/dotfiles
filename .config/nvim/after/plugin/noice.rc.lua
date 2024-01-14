ff35ab21021d41efd885094a05dbf7dfd1caff68
Carlos Grillet <carlosbeta5000@gmail.com>
2024-01-14 15:07:48 +0100

    update: lsp section added to noice

--- .config/nvim/after/plugin/noice.rc.lua
+++ .config/nvim/after/plugin/noice.rc.lua
@@ -2,6 +2,13 @@ local status, noice = pcall(require, "noice")
 if (not status) then return end
 
 noice.setup({
+  lsp = {
+    override = {
+      ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+      ["vim.lsp.util.stylize_markdown"] = true,
+      ["cmp.entry.get_documentation"] = true,
+    },
+  },
   views = {
     cmdline_popup = {
       border = {
