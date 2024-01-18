ae96065335773e38627e8a73c55116d8ab3c4af7
Carlos Grillet <carlosbeta5000@gmail.com>
2024-01-18 12:30:56 +0100

    update: isort added to python formatters

--- .config/nvim/after/plugin/conform.rc.lua
+++ .config/nvim/after/plugin/conform.rc.lua
@@ -1,14 +1,16 @@
 local status, conform = pcall(require, "conform")
-if (not status) then return end
+if not status then
+	return
+end
 
 conform.setup({
-  formatters_by_ft = {
-    javascript = { "prettierd" },
-    lua = { "stylua" },
-    python = { "black" },
-  },
-  format_on_save = {
-    timeout_ms = 500,
-    lsp_fallback = true,
-  },
+	formatters_by_ft = {
+		javascript = { "prettierd" },
+		lua = { "stylua" },
+		python = { "black", "isort" },
+	},
+	format_on_save = {
+		timeout_ms = 500,
+		lsp_fallback = true,
+	},
 })
