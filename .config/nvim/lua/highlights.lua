b981ecfa8ebadd9af5cce13b557e8cfdf82f02ed
Carlos Grillet <carlosbeta5000@gmail.com>
2024-02-20 15:40:49 +0100

    update: ruler form 80 to 100

--- .config/nvim/lua/highlights.lua
+++ .config/nvim/lua/highlights.lua
@@ -1,7 +1,7 @@
 vim.opt.cursorline = true
-vim.opt.colorcolumn = '80'
+vim.opt.colorcolumn = "100"
 vim.opt.termguicolors = true
 vim.opt.winblend = 0
-vim.opt.wildoptions = 'pum'
+vim.opt.wildoptions = "pum"
 vim.opt.pumblend = 5
-vim.opt.background = 'dark'
+vim.opt.background = "dark"
