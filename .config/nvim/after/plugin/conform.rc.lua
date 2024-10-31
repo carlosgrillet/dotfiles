4db214f9de0ddd7520ce6ad996beb8aa54986d5a
Carlos Grillet <carlosbeta5000@gmail.com>
2024-10-31 15:43:56 +0100

    update: ZZ now do qa instead of q

--- .config/nvim/after/plugin/conform.rc.lua
+++ .config/nvim/after/plugin/conform.rc.lua
@@ -1,18 +1,18 @@
 local status, conform = pcall(require, "conform")
 if not status then
-	-- vim.notify("Failed to load conform") -- error in windows
-	return
+  -- vim.notify("Failed to load conform") -- error in windows
+  return
 end
 
 conform.setup({
-	formatters_by_ft = {
-		javascript = { "prettierd" },
-		lua = { "stylua" },
-		python = { "black --skip-string-normalization", "isort" },
-		json = { "prettierd" },
-	},
-	format_on_save = {
-		timeout_ms = 500,
-		lsp_fallback = true,
-	},
+  formatters_by_ft = {
+    javascript = { "prettierd" },
+    lua = { "stylua" },
+    python = { "black --skip-string-normalization", "isort" },
+    json = { "prettierd" },
+  },
+  -- format_on_save = {
+  -- 	timeout_ms = 500,
+  -- 	lsp_fallback = true,
+  -- },
 })
