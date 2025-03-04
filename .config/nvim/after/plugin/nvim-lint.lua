7e3ec64fbb96d2c13cb38edc7e740bfb2eda7f82
Carlos Grillet <carlosbeta5000@gmail.com>
2025-03-04 17:54:45 +0100

    update(lint): lint now also when write to buffer

--- .config/nvim/after/plugin/nvim-lint.lua
+++ .config/nvim/after/plugin/nvim-lint.lua
@@ -8,7 +8,7 @@ lint.linters_by_ft = {
   python = {"pylint"},
 }
 
-vim.api.nvim_create_autocmd({"BufRead", "BufNewFile"}, {
+vim.api.nvim_create_autocmd({"BufRead", "BufNewFile", "BufWrite"}, {
   callback = function()
     lint.try_lint()
   end,
