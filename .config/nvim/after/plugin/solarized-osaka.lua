d890693d70a90e1467d320b9e7b6825016099a45
Carlos Grillet <carlosbeta5000@gmail.com>
2025-03-24 10:40:59 +0100

    update(theme): bg color commented

--- .config/nvim/after/plugin/solarized-osaka.lua
+++ .config/nvim/after/plugin/solarized-osaka.lua
@@ -10,6 +10,22 @@ solarized_osaka.setup({
 		sidebars = "transparent",
 		floats = "transparent",
 	},
+	sidebars = { "qf", "help" },
+	day_brightness = 0.3,
+	hide_inactive_statusline = false,
+	dim_inactive = false,
+	lualine_bold = false,
+
+	on_colors = function(colors)
+		-- colors.bg_float = "#002b36"
+		colors.bg_sidebar = "#002b36"
+		colors.hint = "#FA7334"
+		colors.error = colors.red
+		colors.warning = colors.yellow100
+		colors.info = colors.cyan300
+	end,
+
+	on_highlights = function(highlights, colors) end,
 })
 
 vim.cmd([[colorscheme solarized-osaka]])
