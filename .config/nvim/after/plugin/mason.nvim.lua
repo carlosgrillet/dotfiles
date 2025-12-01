04b10fa5259df8f03ac38e8289556fcac2cf4d82
Carlos Grillet <carlosbeta5000@gmail.com>
2025-12-01 16:19:15 +0100

    add(mason): terraformls required

--- .config/nvim/after/plugin/mason.nvim.lua
+++ .config/nvim/after/plugin/mason.nvim.lua
@@ -1,33 +1,35 @@
 local status, mason = pcall(require, "mason")
 if not status then
-	vim.notify("Failed to load mason")
-	return
+  vim.notify("Failed to load mason")
+  return
 end
-local status2, lspconfig = pcall(require, "mason-lspconfig")
+local status2, mason_lspconfig = pcall(require, "mason-lspconfig")
 if not status2 then
-	vim.notify("Failed to load mason-lspconfig")
-	return
+  vim.notify("Failed to load mason-lspconfig")
+  return
 end
 
 mason.setup({
-	ui = {
-		icons = {
-			package_installed = "✓",
-			package_pending = "➜",
-			package_uninstalled = "✗",
-		},
-	},
+  ui = {
+    icons = {
+      package_installed = "✓",
+      package_pending = "➜",
+      package_uninstalled = "✗",
+    },
+  },
 })
 
-lspconfig.setup({
-	ensure_installed = {
-		"bashls",   -- bash
-		"dockerls", -- Dockerfile
-		"lua_ls",   -- lua
-		-- "pylsp",    -- pyhton
-    "gopls",    -- go
-	},
-	automatic_installation = true,
+mason_lspconfig.setup({
+  ensure_installed = {
+    "bashls", -- bash
+    "dockerls", -- Dockerfile
+    "lua_ls", -- lua
+    "clangd",
+    "pyright",
+    "ruff",
+    "terraformls"
+  },
+  automatic_installation = true,
 })
 
-vim.keymap.set("n", ";m", ":Mason<Return>", {silent = true})
+vim.keymap.set("n", ";m", ":Mason<Return>", { silent = true })
