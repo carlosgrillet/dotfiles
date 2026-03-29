866b208206077ba67b8931170bd34a347e52d892
Carlos Grillet <carlosbeta5000@gmail.com>
2026-03-29 14:39:20 +0200

    feat: new path HOME/go/bin

--- .zshrc.d/env.sh
+++ .zshrc.d/env.sh
@@ -5,5 +5,6 @@ export COLORTERM="truecolor"
 export EDITOR='nvim'
 export VISUAL=$EDITOR
 export PATH="$PATH:/usr/local/go/bin"
+export PATH="$PATH:$HOME/go/bin"
 export PATH="$PATH:$HOME/.local/bin"
 export PYTHONDONTWRITEBYTECODE=1
