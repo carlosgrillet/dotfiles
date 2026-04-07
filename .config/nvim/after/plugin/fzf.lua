1930770238def085bcfe3035d4c695d1c98d0ac1
Carlos Grillet <carlosbeta5000@gmail.com>
2026-04-07 20:57:22 +0200

    feat(fzf): hide rust files

--- .config/nvim/after/plugin/fzf.lua
+++ .config/nvim/after/plugin/fzf.lua
@@ -1,39 +1,49 @@
 local status, fzf_lua = pcall(require, "fzf-lua")
 if not status then
-	vim.notify("Failed to load fzf-lua")
-	return
+  vim.notify("Failed to load fzf-lua")
+  return
 end
 
 fzf_lua.setup {
   files = {
-    cwd_prompt= false,
+    cwd_prompt = false,
     git_icons = true,
+    fd_opts = "--color=never --type f --hidden --follow"
+        .. " --exclude .git"
+        .. " --exclude target"
+        .. " --exclude '*.lock'",
+  },
+  grep = {
+    rg_opts = "--column --line-number --no-heading --color=always --smart-case"
+        .. " --glob '!.git'"
+        .. " --glob '!target'"
+        .. " --glob '!*.lock'",
   },
 }
 
 -- Keymaps
 vim.keymap.set("n", ";F", function()
-	fzf_lua.builtin()
+  fzf_lua.builtin()
 end)
 
 vim.keymap.set("n", ";f", function()
-	fzf_lua.files()
+  fzf_lua.files()
 end)
 
 vim.keymap.set("n", ";g", function()
-	fzf_lua.live_grep_native()
+  fzf_lua.live_grep_native()
 end)
 
 vim.keymap.set("v", ";G", function()
-	fzf_lua.grep_visual()
+  fzf_lua.grep_visual()
 end)
 
 vim.keymap.set("n", ";d", function()
-	fzf_lua.diagnostics_document()
+  fzf_lua.diagnostics_document()
 end)
 
 vim.keymap.set("n", ";b", function()
-	fzf_lua.buffers()
+  fzf_lua.buffers()
 end)
 
 vim.keymap.set("n", ";;", function()
