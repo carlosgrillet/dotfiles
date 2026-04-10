73b3d51860e3c281bbda6b9d0cbc4242cfb547a2
Carlos Grillet <carlosbeta5000@gmail.com>
2026-04-10 23:54:31 +0200

    fix: fixing anoying autocompletion when pressing enter

--- .config/lazynvim/lua/plugins/editor.lua
+++ .config/lazynvim/lua/plugins/editor.lua
@@ -24,7 +24,7 @@ return {
           ["<C-e>"] = cmp.mapping.close(),
           ["<CR>"] = cmp.mapping.confirm({
             behavior = cmp.ConfirmBehavior.Replace,
-            select = true,
+            select = false,
           }),
         }),
         sources = cmp.config.sources({
