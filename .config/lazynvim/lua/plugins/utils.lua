d8bf178c2e62ec5d7a0c8aa86059f7a90821c81d
Carlos Grillet <carlosbeta5000@gmail.com>
2026-04-26 18:57:11 +0200

    deprecate: gitsigns commands dprecated for hunk navegation

--- .config/lazynvim/lua/plugins/utils.lua
+++ .config/lazynvim/lua/plugins/utils.lua
@@ -1,24 +1,26 @@
 return {
-  {
-    "lewis6991/gitsigns.nvim",
-    opts = {
-      signs = { delete = { icon = '┃' } },
-      signs_staged = { delete = { text = '┃' } },
-      signcolumn = true,
-      numhl = true,
-      on_attach = function(bufnr)
-        local gitsigns = require('gitsigns')
-        vim.keymap.set('n', 'Sh', gitsigns.preview_hunk, { buffer = bufnr })
-        vim.keymap.set('n', 'sh', gitsigns.stage_hunk, { buffer = bufnr })
-        vim.keymap.set('n', 'Rh', gitsigns.reset_hunk, { buffer = bufnr })
-        vim.keymap.set('n', 'gb', gitsigns.blame_line, { buffer = bufnr })
-        vim.keymap.set('n', ']', gitsigns.next_hunk, { buffer = bufnr })
-        vim.keymap.set('n', '[', gitsigns.prev_hunk, { buffer = bufnr })
-        vim.keymap.set('n', 'vih', gitsigns.select_hunk, { buffer = bufnr })
-      end,
-    }
-  },
-  {
-    "wakatime/vim-wakatime",
-  },
+    {
+        "lewis6991/gitsigns.nvim",
+        opts = {
+            signs = { delete = { icon = '┃' } },
+            signs_staged = { delete = { text = '┃' } },
+            signcolumn = true,
+            numhl = true,
+            on_attach = function(bufnr)
+                local gitsigns = require('gitsigns')
+                vim.keymap.set('n', 'Sh', gitsigns.preview_hunk, { buffer = bufnr })
+                vim.keymap.set('n', 'sh', gitsigns.stage_hunk, { buffer = bufnr })
+                vim.keymap.set('n', 'Rh', gitsigns.reset_hunk, { buffer = bufnr })
+                vim.keymap.set('n', 'gb', gitsigns.blame_line, { buffer = bufnr })
+                vim.keymap.set('n', 'vih', gitsigns.select_hunk, { buffer = bufnr })
+            end,
+        }
+    },
+    {
+        "wakatime/vim-wakatime",
+        lazy = false,
+        opts = {
+            status_bar_enabled = false,
+        },
+    },
 }
