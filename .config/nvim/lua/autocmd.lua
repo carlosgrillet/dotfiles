0852fbe97606d5158edaa8e1108f53eda614475e
Carlos Grillet <carlosbeta5000@gmail.com>
2026-06-02 10:06:00 +0200

    chore(autocmd): move linux autocmd to single autocmd

--- .config/nvim/lua/autocmd.lua
+++ .config/nvim/lua/autocmd.lua
@@ -30,15 +30,12 @@ vim.api.nvim_create_autocmd({ "BufEnter", "BufNewFile" }, {
     opt.shiftwidth  = 8
     opt.softtabstop = 8
     opt.expandtab   = false
-    opt.textwidth   = 79
+    opt.textwidth   = 80
     opt.colorcolumn = "80"
     opt.list        = true
     opt.listchars   = {
-      tab      = "».",
-      trail    = ".",
-      extends  = ">",
-      precedes = "<",
-      nbsp     = "+",
+      tab      = "  ",
+      trail    = " ",
     }
     opt.wrap        = false
     opt.number      = true
@@ -52,27 +49,14 @@ vim.api.nvim_create_autocmd({ "BufEnter", "BufNewFile" }, {
     opt.cinoptions = ":0,l1,t0,g0,(0"
     opt.spelllang  = "en_us"
     opt.spell      = false
-  end,
-})
 
--- Highlight trailing whitespace (forbidden in kernel patches)
-vim.api.nvim_create_autocmd({ "BufEnter", "BufNewFile" }, {
-  group = kernel_augroup,
-  pattern = { "*.c", "*.h", "*.cpp", "*.cc", "*.cxx", "*.hpp", "*.S", "*.s" },
-  desc = "Highlight trailing whitespace",
-  callback = function()
+    -- Highlight trailing whitespace
     vim.cmd("highlight TrailingWhitespace ctermbg=red guibg=#FF0000")
     vim.cmd("match TrailingWhitespace /\\s\\+$/")
-  end,
-})
 
--- Highlight characters beyond column 80
-vim.api.nvim_create_autocmd({ "BufEnter", "BufNewFile" }, {
-  group = kernel_augroup,
-  pattern = { "*.c", "*.h", "*.cpp", "*.cc", "*.cxx", "*.hpp", "*.S", "*.s" },
-  desc = "Highlight overlong lines",
-  callback = function()
+    -- Highlight overlong lines
     vim.cmd("highlight OverLength ctermbg=darkred ctermfg=white guibg=#592929")
     vim.cmd("2match OverLength /\\%81v.\\+/")
+
   end,
 })
